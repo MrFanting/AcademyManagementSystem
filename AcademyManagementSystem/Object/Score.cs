578bca3b2273eb1b11960b1dbbee275cb824b9f8
13126692900 <13126692900@sina.cn>
2018-07-03 16:41:30 +0800

    类的建立

---
+++ AcademyManagementSystem/Object/Score.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AcademyManagementSystem.Object
+{
+    class Score
+    {
+        private string con;
+        private string studentName;
+        private string mark;
+
+        public string Con { get => con; set => con = value; }
+        public string StudentName { get => studentName; set => studentName = value; }
+        public string Mark { get => mark; set => mark = value; }
+    }
+}
---
+++ AcademyManagementSystem/Object/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AcademyManagementSystem.Object
+{
+    class Teacher
+    {
+        private string id;
+        private string name;
+        private string gender;
+        private string age;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string Age { get => age; set => age = value; }
+    }
+}
